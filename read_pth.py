7dd8e9760516683a6d26a2bfd30fdce4e24dd20d
Qihang Liu <l2982124385@gmail.com>
2025-10-21 14:59:05 +0800

    读取.pth文件脚本 希望修改成读取所有文件的脚本

---
+++ read_pth.py
@@ -0,0 +1,35 @@
+import torch
+import pprint
+
+##===================================================
+###可以使用
+#####检查变量文件类型：isinstance(variable,dtype)
+#####字典的每个元素调用：.items()
+#####按键名调用字典内部元素:dict.get('keyname')
+##===================================================
+###用于记录
+#####pytorch1.9的.pth文件类型是dict
+
+
+torch_file_path = '../checkpoint.pth'
+
+
+def show_all_keyname(dict):
+    for key,_ in dict.items():
+            print('key_name:',key)
+
+
+def read_torch_file():
+    model = torch.load(torch_file_path)
+    bool = isinstance(model,dict)
+    pprint.pprint(bool)
+    return model
+    
+def read_key(dict,keyname):
+    data = dict.get(keyname)
+    pprint.pprint(data)
+
+
+if __name__ == '__main__':
+    model = read_torch_file()
+    read_key(model,'cfg')
